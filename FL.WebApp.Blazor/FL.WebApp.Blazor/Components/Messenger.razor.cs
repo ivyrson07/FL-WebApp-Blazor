6797f4e9e46511fc91dfce3a44a60d32b7f66458
Ivyrson Garcia <ivyrsongarcia17@gmail.com>
2023-09-15 01:23:24 +0800

    Initial commit.

---
+++ FL.WebApp.Blazor/FL.WebApp.Blazor/Components/Messenger.razor.cs
@@ -0,0 +1,72 @@
+﻿using EasyNetQ;
+using FL.Common.Models;
+using FL.WebApp.Blazor.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace FL.WebApp.Blazor.Components
+{
+    public partial class Messenger : ComponentBase
+    {
+        [Inject]
+        private ICommentService? CommentApi { get; set; }
+
+        [Inject]
+        private IBus Bus { get; set; }
+
+        public string comment = "";
+        private List<string> incomingComments = new List<string>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            try
+            {
+                await Bus.PubSub.SubscribeAsync<Comment>("comment", message => Task.Factory.StartNew(async () =>
+                {
+                    if (!string.IsNullOrWhiteSpace(message.Description))
+                    {
+                        incomingComments.Add(message.Description);
+
+                        await InvokeAsync(() =>
+                        {
+                            comment = "";
+                            StateHasChanged();
+                        });
+                    }
+                }).ContinueWith(task =>
+                {
+                    if (task.IsCompleted && !task.IsFaulted)
+                    {
+                    }
+                }));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error creating EasyNetQ bus: {ex.Message}");
+                Console.WriteLine($"Error creating EasyNetQ bus: {ex.StackTrace}");
+            }
+        }
+
+        private async Task Send()
+        {
+            await CommentApi.CreateAsync(new Comment
+            {
+                PostId = Guid.NewGuid(),
+                Description = comment
+            });
+        }
+
+        private async Task SendHandleKeyPress(KeyboardEventArgs e)
+        {
+            await Task.Run(async () =>
+            {
+                if (e.Key == "Enter")
+                {
+                    await Send();
+                }
+            });
+        }
+    }
+}
---
+++ FL.WebApp.Blazor/FL.WebApp.Blazor/Services/CommentService.cs
@@ -0,0 +1,70 @@
+﻿using FL.Common.Models;
+using Newtonsoft.Json;
+
+namespace FL.WebApp.Blazor.Services
+{
+    public interface ICommentService
+    {
+        Task<Comment> GetAsync(Guid id);
+
+        Task<Comment> CreateAsync(Comment comment);
+    }
+
+    public class CommentService : ICommentService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CommentService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<Comment> GetAsync(Guid id)
+        {
+            var response = await _httpClient.GetFromJsonAsync<Comment>($"https://localhost:44349/comments/{id}");
+
+            if (response != null)
+                return response;
+            else
+            {
+                Console.WriteLine($"Response is null");
+
+                return new Comment();
+            }
+        }
+
+        public async Task<Comment> CreateAsync(Comment comment)
+        {
+            try
+            {
+                var response = await _httpClient.PostAsJsonAsync("https://localhost:44349/comments/create", new Comment
+                {
+                    PostId = Guid.NewGuid(),
+                    Description = comment.Description
+                });
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    var incomingComment = JsonConvert.DeserializeObject<Comment>(responseString);
+
+                    if (incomingComment != null && incomingComment.Description != null)
+                    {
+                        return new Comment
+                        {
+                            PostId = incomingComment.PostId,
+                            Description = incomingComment.Description
+                        };
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error on API call /comments/create : {ex.Message}");
+                Console.WriteLine($"Error on API call /comments/create : {ex.StackTrace}");
+            }
+
+            return new Comment();
+        }
+    }
+}
